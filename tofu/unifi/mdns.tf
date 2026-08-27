4ee4ccb151d8798eb43d34ace23bb569f9a99077
Angel Penchev <angel@penchev.dev>
2026-08-28 02:54:30 +0300

    feat(unifi): import global settings and mdns specs

--- tofu/unifi/mdns.tf
+++ tofu/unifi/mdns.tf
@@ -3,97 +3,32 @@
 # ----------------------------------------------------------------------------
 # Note: As of v0.55.0, the ubiquiti-community/unifi provider does not yet have 
 # a native `unifi_setting_mdns` resource for granular mDNS service filtering.
-#
-# PR in progress: N/A
 # ----------------------------------------------------------------------------
 
 /*
-resource "unifi_setting_mdns" "site_services" {
+resource "unifi_setting_mdns" "gateway_proxy" {
   site = "default"
 
-  service {
-    code      = "amazon_devices"
-    name      = "Amazon devices"
-    addresses = [
-      "_amzn-wplay._tcp.local",
-      "_amazonecho-remote._tcp",
-      "_workstation._tcp.local"
-    ]
-  }
+  mode = "custom"
+  
+  vlan_scope = [
+    unifi_network.main.id,
+    unifi_network.guest.id,
+    unifi_network.iot.id
+  ]
+  
+  service_scope = "specific"
 
-  service {
-    code      = "apple_airPlay"
-    name      = "Apple AirPlay"
-    addresses = [
-      "_companion-link._tcp.local",
-      "_appletv-v2._tcp.local",
-      "_raop._tcp.local",
-      "_airplay._tcp.local"
-    ]
-  }
-
-  service {
-    code      = "google_chromecast"
-    name      = "Google Chromecast"
-    addresses = [
-      "_googlecast._tcp.local",
-      "_googlezone._tcp.local"
-    ]
-  }
-
-  service {
-    code      = "homeKit"
-    name      = "HomeKit"
-    addresses = [
-      "_homekit._tcp.local",
-      "_hap._tcp.local"
-    ]
-  }
-
-  service {
-    code      = "matter_network"
-    name      = "Matter network"
-    addresses = [
-      "_matterd._udp",
-      "_matter_gateway._tcp",
-      "_matter._tcp",
-      "_matterc._udp"
-    ]
-  }
-
-  service {
-    code      = "philips_hue"
-    name      = "Philips Hue"
-    addresses = [
-      "_philipshue._tcp.local"
-    ]
-  }
-
-  service {
-    code      = "shelly"
-    name      = "Shelly"
-    addresses = [
-      "_shelly._tcp.local"
-    ]
-  }
-
-  service {
-    code      = "sonos"
-    name      = "Sonos"
-    addresses = [
-      "_sonos._tcp.local"
-    ]
-  }
-
-  service {
-    code      = "spotify_connect"
-    name      = "Spotify Connect"
-    addresses = [
-      "_spotify-connect._tcp.local"
-    ]
-  }
-
-  # Add other services like android_tv_remote, printers, scanners, etc. 
-  # following the same schema block format.
+  # The UI shows 17 selected services including Apple suite, Amazon, Android, etc.
+  services = [
+    "amazon_devices",
+    "android_tv_remote",
+    "apple_airdrop",
+    "apple_airplay",
+    "apple_file_sharing",
+    "apple_ichat",
+    "apple_itunes",
+    # ... and 10 more
+  ]
 }
 */
---
+++ tofu/unifi/settings.tf
@@ -0,0 +1,39 @@
+resource "unifi_setting" "default" {
+  site = "default"
+
+  # Manages the site-wide settings that the provider supports.
+  igmp_snooping = {
+    enabled = false
+  }
+}
+
+# ----------------------------------------------------------------------------
+# Global Switch Settings & Security Posture
+# ----------------------------------------------------------------------------
+# Note: As of v0.55.0, the ubiquiti-community/unifi provider does not natively 
+# support the new UniFi Global Switch Settings and Default Security Posture.
+# ----------------------------------------------------------------------------
+/*
+resource "unifi_setting_switch" "global" {
+  site = "default"
+
+  spanning_tree               = "rstp"
+  jumbo_frames                = false
+  rogue_dhcp_server_detection = true
+  
+  l3_network_isolation = false
+  device_isolation     = false
+
+  dot1x_control {
+    enabled           = true
+    credential_source = "local"
+    fallback_vlan_id  = unifi_network.guest.id
+  }
+}
+
+resource "unifi_setting_security" "posture" {
+  site = "default"
+
+  default_security_posture = "allow_all"
+}
+*/
