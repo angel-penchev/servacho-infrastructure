b74faa9879011c5943aa78d959cc2c3a5ba6373e
Angel Penchev <angel@penchev.dev>
2026-08-27 08:29:19 +0300

    feat(#11): define and import unifi devices

---
+++ tofu/unifi/devices.tf
@@ -0,0 +1,49 @@
+resource "unifi_device" "udm_pro_max" {
+  mac  = "28:70:4e:5c:b4:b2"
+  name = "Dream Machinacho Pro Max"
+}
+
+resource "unifi_device" "usw_aggregation" {
+  mac  = "1c:6a:1b:98:38:ee"
+  name = "USW Aggregation"
+}
+
+resource "unifi_device" "usw_pro_max_24_poe" {
+  mac  = "9c:05:d6:e2:6b:1d"
+  name = "USW Pro Max 24 PoE"
+}
+
+resource "unifi_device" "u7_pro_bedroom" {
+  mac  = "9c:05:d6:d9:af:65"
+  name = "Bedroom U7-Pro"
+}
+
+resource "unifi_device" "u7_pro_living_room" {
+  mac  = "9c:05:d6:d9:ad:79"
+  name = "Living Room U7-Pro"
+}
+
+import {
+  to = unifi_device.udm_pro_max
+  id = "28:70:4e:5c:b4:b2"
+}
+
+import {
+  to = unifi_device.usw_aggregation
+  id = "1c:6a:1b:98:38:ee"
+}
+
+import {
+  to = unifi_device.usw_pro_max_24_poe
+  id = "9c:05:d6:e2:6b:1d"
+}
+
+import {
+  to = unifi_device.u7_pro_bedroom
+  id = "9c:05:d6:d9:af:65"
+}
+
+import {
+  to = unifi_device.u7_pro_living_room
+  id = "9c:05:d6:d9:ad:79"
+}
