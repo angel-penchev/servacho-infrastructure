f6d799c207a96370a7d05d26c309e5dd9b0c484e
Angel Penchev <angel@penchev.dev>
2026-08-27 08:47:00 +0300

    feat(unifi): configure advanced switch settings and disable flags

--- tofu/unifi/devices.tf
+++ tofu/unifi/devices.tf
@@ -2,24 +2,34 @@ resource "unifi_device" "udm_pro_max" {
   mac               = "28:70:4e:5c:b4:b2"
   name              = "Dream Machinacho Pro Max"
   forget_on_destroy = false
+  disabled          = false
 }
 
 resource "unifi_device" "usw_aggregation" {
   mac               = "1c:6a:1b:98:38:ee"
   name              = "USW Aggregation"
   forget_on_destroy = false
+  disabled          = false
 }
 
 resource "unifi_device" "usw_pro_max_24_poe" {
-  mac               = "9c:05:d6:e2:6b:1d"
-  name              = "USW Pro Max 24 PoE"
-  forget_on_destroy = false
+  mac                = "9c:05:d6:e2:6b:1d"
+  name               = "USW Pro Max 24 PoE"
+  forget_on_destroy  = false
+  disabled           = false
+  flowctrl_enabled   = false
+  jumboframe_enabled = false
+
+  config_network = {
+    type = "dhcp"
+  }
 }
 
 resource "unifi_device" "u7_pro_bedroom" {
   mac               = "9c:05:d6:d9:af:65"
   name              = "Bedroom U7-Pro"
   forget_on_destroy = false
+  disabled          = false
 }
 
 resource "unifi_device" "u7_pro_living_room" {
